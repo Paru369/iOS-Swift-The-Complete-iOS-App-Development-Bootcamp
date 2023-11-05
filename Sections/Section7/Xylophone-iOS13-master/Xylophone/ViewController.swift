adb2e2866cfa613f34024fd0d56d47740ea2c30f
parutech <paruvitu@gmail.com>
2023-11-05 14:43:22 -0300

    feat: section 7 - solved

--- Sections/Section7/Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Sections/Section7/Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -7,15 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
+    func playSound(note: String) {
+        guard let path = Bundle.main.path(forResource: note, ofType:"wav") else {
+            return
+            
+        }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+ 
+    @IBAction func keyCPressed(_ sender: UIButton) {
+        playSound(note: "C")
+    }
     
-
+    @IBAction func keyDPressed(_ sender: UIButton) {
+        playSound(note: "D")
+    }
+    
+    @IBAction func keyEPressed(_ sender: UIButton) {
+        playSound(note: "E")
+    }
+    
+    @IBAction func keyFPressed(_ sender: UIButton) {
+        playSound(note: "F")
+    }
+    
+    @IBAction func keyGPressed(_ sender: UIButton) {
+        playSound(note: "G")
+    }
+    
+    @IBAction func keyAPressed(_ sender: UIButton) {
+        playSound(note: "A")
+    }
+    
+    @IBAction func keyBPressed(_ sender: UIButton) {
+        playSound(note: "B")
+    }
+    
 }
 
