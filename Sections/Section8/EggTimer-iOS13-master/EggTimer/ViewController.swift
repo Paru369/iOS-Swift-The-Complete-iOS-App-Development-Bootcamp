3174a3d655cb3c9639f29226186437c403cd8e2d
parutech <paruvitu@gmail.com>
2023-11-06 13:56:47 -0300

    Section 8 add done title

--- Sections/Section8/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ Sections/Section8/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,27 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTime = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTime = ["Soft": 1, "Medium": 2, "Hard": 3]
     var secondsRemaining = 60
+    var timer = Timer()
+
+    
+    @IBOutlet weak var titleLable: UILabel!
     
     
     @IBAction func haradnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
         
       let hardness = sender.currentTitle!
         
       let minutes = eggTime[hardness]!
         secondsRemaining = minutes * 60
         
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-                if self.secondsRemaining > 0 {
-                    print ("\(self.secondsRemaining) seconds")
-                    self.secondsRemaining -= 1
-                } else {
+       timer =  Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+           if self.secondsRemaining > 0 {
+               print ("\(self.secondsRemaining) seconds")
+               self.secondsRemaining -= 1
+              
+               if self.secondsRemaining == 0 {
+                   self.titleLable.text = "Cabooo"
+               }
+               
+           } else {
                     Timer.invalidate()
                 }
             }
-        
+      
     }
+    
+    
+    
+    
     
 
 }
