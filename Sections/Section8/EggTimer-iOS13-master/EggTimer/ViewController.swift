bfc9b2579c1028dfe58ccf95ec35844ae5ff092d
parutech <paruvitu@gmail.com>
2023-11-06 13:45:23 -0300

    Section 8 add timer

--- Sections/Section8/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ Sections/Section8/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,25 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTime = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var secondsRemaining = 60
+    
     
     @IBAction func haradnessSelected(_ sender: UIButton) {
         
       let hardness = sender.currentTitle!
         
-        switch hardness {
-            
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("Error...")
-        }
+      let minutes = eggTime[hardness]!
+        secondsRemaining = minutes * 60
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+                if self.secondsRemaining > 0 {
+                    print ("\(self.secondsRemaining) seconds")
+                    self.secondsRemaining -= 1
+                } else {
+                    Timer.invalidate()
+                }
+            }
+        
     }
     
 
