6cc987aafd5629c145686dea50ef9a2c683d5991
parutech <paruvitu@gmail.com>
2023-11-06 22:09:51 -0300

    Section 8 add progress bar

--- Sections/Section8/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ Sections/Section8/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -13,37 +13,49 @@ class ViewController: UIViewController {
     let eggTime = ["Soft": 1, "Medium": 2, "Hard": 3]
     var secondsRemaining = 60
     var timer = Timer()
-
+    var timeElapsed = 0
     
     @IBOutlet weak var titleLable: UILabel!
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var timerLabel: UILabel!
     
     @IBAction func haradnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
-        
+        timeElapsed = 0
+        titleLable.text = ""
       let hardness = sender.currentTitle!
-        
       let minutes = eggTime[hardness]!
         secondsRemaining = minutes * 60
         
-       timer =  Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+        
+        
+        
+        timer =  Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
            if self.secondsRemaining > 0 {
                print ("\(self.secondsRemaining) seconds")
                self.secondsRemaining -= 1
-              
+               self.timeElapsed += 1
+               self.progressBar.progress = Float(self.timeElapsed) / Float(60 * minutes)
+               self.timerLabel.text = "\(self.secondsRemaining)"
+               self.titleLable.text = hardness
+               
+               
                if self.secondsRemaining == 0 {
-                   self.titleLable.text = "Cabooo"
+                   self.titleLable.text = "DONE"
                }
                
            } else {
                     Timer.invalidate()
                 }
             }
-      
+        
     }
     
     
+   
     
     
     
