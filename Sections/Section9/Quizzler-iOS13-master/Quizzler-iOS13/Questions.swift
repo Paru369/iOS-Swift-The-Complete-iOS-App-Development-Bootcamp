6d656a709deccec25d4bca254d3d333fdcd81305
parutech <paruvitu@gmail.com>
2023-11-08 17:56:38 -0300

    feat: section 9

---
+++ Sections/Section9/Quizzler-iOS13-master/Quizzler-iOS13/Questions.swift
@@ -0,0 +1,15 @@
+//
+//  Questions.swift
+//  Quizzler-iOS13
+//
+//  Created by Paulo Pinheiro on 11/8/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Questions {
+    let text: String
+    let answer: String
+}
