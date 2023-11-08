a3f6a259e00e07ae70edc3601044087da9728c63
parutech <paruvitu@gmail.com>
2023-11-07 22:44:29 -0300

    feat: section 9

--- Sections/Section9/Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
+++ Sections/Section9/Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
@@ -19,18 +19,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     
+    let quiz = [
+    ["O Brasil tem palmeiras?", "True"],
+    ["Isso é um Quiz?", "True"],
+    ["Existe array 2x2?", "True"],
+    ["Isso é uma pergunta?", "False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        questionLabel.text = "Vegan is cool?"
+        updateUI()
     }
 
 
-    @IBAction func answerButtonPressed(_ sender: Any) {
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        print(userAnswer)
+        let actualAnser = quiz[questionNumber][1]
+        
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+            
+        } else {
+            questionNumber = 0
+        }
+        
+        updateUI()
+        
+        if userAnswer == actualAnser {
+            print("OK")
+        } else {
+            print("Cagou")
+        }
     }
     
-    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+
+    }
     
 }
 
