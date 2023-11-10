3b65c70c75329b65440724f5e7496d821a4b7fa1
parutech <paruvitu@gmail.com>
2023-11-10 14:06:06 -0300

    feat: section 10

--- Sections/Section10/Destini-iOS13-master/Destini-iOS13/Controller/ViewController.swift
+++ Sections/Section10/Destini-iOS13-master/Destini-iOS13/Controller/ViewController.swift
@@ -15,9 +15,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice2Button: UIButton!
     
     
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+
+    let example = Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right.")
+    let example2 = Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead")
+    let example3 = Story(title: "You find a teasure chest", choice1: "Open it.", choice2: "Check for traps.")
     
+    var stories: Array<Any>
+    
+    init(stories: Array<Any>) {
+   
+        self.stories = stories
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateView() {
+        storyLabel.text = "You see a fork in the road."
+        choice1Button.setTitle("Take a left.", for: .normal)
+        choice2Button.setTitle("Take a right.", for: .normal)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateView()
 
     }
 
