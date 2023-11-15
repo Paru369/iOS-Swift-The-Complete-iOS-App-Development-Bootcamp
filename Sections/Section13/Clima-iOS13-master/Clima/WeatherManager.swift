2f5fc24a6a1e3d5ab955934309bb2ad410224145
parutech <paruvitu@gmail.com>
2023-11-15 11:05:24 -0300

    feat: section13 add api

--- Sections/Section13/Clima-iOS13-master/Clima/WeatherManager.swift
+++ Sections/Section13/Clima-iOS13-master/Clima/WeatherManager.swift
@@ -13,7 +13,6 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
         performRequest(urlString: urlString)
     }
     
@@ -21,27 +20,35 @@ struct WeatherManager {
         // 1. Create a URL
         
         if let url = URL(string: urlString) {
-            print("-> \(url)")
             // 2. Create a URLSessopm
             let session = URLSession(configuration: .default)
             // 3 . Give the session a taks
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             // 4 . Start the taks
             task.resume()
         }
         
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+             let decoderData = try decoder.decode(WeatherData.self, from: weatherData)
+            print("=> \(decoder.self)")
+            print(decoderData.weather[0].description)
+        } catch {
+            print(error)
         }
-        
-        if let safeData = data {
-            print("---> \(safeData)")
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
         }
-    }
+    
+ 
 }
