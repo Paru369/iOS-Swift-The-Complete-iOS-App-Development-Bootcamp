71737e7e60f5e977bfa24cf6af7d7e92427f8d4d
parutech <paruvitu@gmail.com>
2023-11-15 22:04:32 -0300

    feat: section13 add location manager

--- Sections/Section13/Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
+++ Sections/Section13/Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
 
@@ -16,11 +17,23 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var searchTextField: UITextField!
     
     var weatherManager = WeatherManager()
-
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
         searchTextField.delegate = self
         weatherManager.delegate = self
+       
+        
+        
+        
+        
+        
     }
 }
 
@@ -72,4 +85,19 @@ extension WeatherViewController: WeatherManagerDelegate {
         print(error)
     }
     
+}
+
+//MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("got location")
+        print("Tete")
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
