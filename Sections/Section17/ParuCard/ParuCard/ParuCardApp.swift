3d2f0a539f76183914287a16434873f6e58c2d74
parutech <paruvitu@gmail.com>
2023-11-23 20:56:08 -0300

    section 17 add new project

---
+++ Sections/Section17/ParuCard/ParuCard/ParuCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParuCardApp.swift
+//  ParuCard
+//
+//  Created by Paulo Pinheiro on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ParuCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
