4bc1ffe7e241d82a11a525cf66abdd33bb240984
parutech <paruvitu@gmail.com>
2023-11-27 14:26:51 -0300

    feat: section19 change itemArray to a Item Object

--- Sections/Section19/Todoey-iOS13-master/Todoey/Controller/TodoListViewController.swift
+++ Sections/Section19/Todoey-iOS13-master/Todoey/Controller/TodoListViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let newItem = Item()
+        newItem.title = "Find my key"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Teste new Item"
+        itemArray.append(newItem2)
+        
+        
+//        if let items =  defaults.array(forKey: "TodoListArray") as? [String] {
+//            itemArray = items
+//        }
+    }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -20,9 +41,13 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
             if textField.text != "" {
-                self.itemArray.append(textField.text!)
+                let newItem = Item()
+                newItem.title = textField.text!
+                self.itemArray.append(newItem)
                 self.tableView.reloadData()
             }
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             print(self.itemArray)
         }
         
@@ -35,18 +60,15 @@ class TodoListViewController: UITableViewController {
     }
     
     
-    var itemArray = ["Find my key", "Buy eggs", "Destroy Demongorgon"]
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+
+  
     
     //MARK: - Tableview DataSource Methods
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
         
         return cell
     }
---
+++ Sections/Section19/Todoey-iOS13-master/Todoey/Data Models/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Paulo Pinheiro on 11/27/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
